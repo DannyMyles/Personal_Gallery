f9d91633c315ffee8bc20542973ea63368f13326
DannyMyles <dancun.wawire@student.moringaschool.com>
2021-11-27 23:24:50 +0300

    added models

--- photoshare/photos/models.py
+++ photoshare/photos/models.py
@@ -1,3 +1,12 @@
-from django.db import models
+from django.urls import path
+from . import views
 
-# Create your models here.
+urlpatterns = [
+    path('login/', views.loginUser, name="login"),
+    path('logout/', views.logoutUser, name="logout"),
+    path('register/', views.registerUser, name="register"),
+
+    path('', views.gallery, name='gallery'),
+    path('photo/<str:pk>/', views.viewPhoto, name='photo'),
+    path('add/', views.addPhoto, name='add'),
+]
